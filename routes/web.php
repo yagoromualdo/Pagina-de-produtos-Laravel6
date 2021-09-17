8b89071cb32f5ad46ed055bf2a62bc9706b5192b
Yago Romualdo <yagoromualdo.yr@gmail.com>
2021-09-17 17:13:42 -0300

    Aula 14 finished

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/hello', function () {
     return 'Hello my friend';
 });
@@ -19,7 +23,57 @@ Route::get('/teste', function () {
     return view('teste');
 });
 
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello/{flag}', function ($flag) {
+    return "Hello my friend {$flag}";
 });
+
+Route::get('/testeid/{id}', function ($id) {
+    return "O id é: {$id}";
+});
+
+Route::get('redirect1', function () {
+    return redirect('/redirect2');
+});
+
+Route::get('redirect2', function () {
+    return 'redirect 2';
+});
+
+Route::get('/login', function () {
+    return 'Tela de login';
+})->name('login');
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+
+    Route::get('/financeiro', 'TesteController@teste')->name('admin.financeiro');
+
+    Route::get('/produtos', 'TesteController@teste')->name('admin.products');
+
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+
+//             OU          //
+
+/*
+Route::middleware([])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::namespace('Admin')->group(function () {
+            Route::name('admin.')->group(function () {
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+                Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                Route::get('/produtos', 'TesteController@teste')->name('products');
+                Route::get('/', function () {
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+            });
+        });
+    });
+});
+*/
